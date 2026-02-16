4f380a6a02f9dea2c04b8889982bd9048696ceb3
Adistian Herlambang <adistian59@gmail.com>
2026-02-16 20:41:32 +0700

    nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./Navbar.css";
+
+import { NavLogo, FooterLogo } from "./Logo";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Navbar() {
+  const navRef = useRef(null);
+  const bgRef = useRef(null);
+
+  const [tes, setTest] = useState("")
+
+  useEffect(() => {
+    const nav = navRef.current;
+    const bg = bgRef.current;
+    const items = nav.querySelectorAll(".navItem");
+
+    function moveBackground(target) {
+      const rect = target.getBoundingClientRect();
+      const parentRect = target.parentElement.getBoundingClientRect();
+
+      gsap.to(bg, {
+        x: rect.left - parentRect.left,
+        width: rect.width,
+        duration: 1,
+        ease: "power3.out"
+      });
+
+      // Ubah warna nav-item menjadi white untuk item yang aktif
+      items.forEach((item) => {
+        if (item === target) {
+          gsap.to(item, {
+            color: "#ffffff",
+            duration: 1,
+            ease: "power3.out"
+          });
+        } else {
+          gsap.to(item, {
+            color: "#000",
+            duration: 1,
+            ease: "power3.out"
+          });
+        }
+      });
+    }
+
+    items.forEach((item) => {
+      const id = item.getAttribute("href");
+
+      ScrollTrigger.create({
+        trigger: id,
+        start: "top center",
+        end: "bottom center",
+        onEnter: () => moveBackground(item),
+        onEnterBack: () => moveBackground(item)
+      });
+
+      item.addEventListener("click", () => {
+        moveBackground(item);
+      });
+    });
+
+    // set default ke item pertama
+    if (items.length > 0) {
+      moveBackground(items[0]);
+    }
+
+  }, []);
+
+  return (
+    <nav className="nav">
+      <NavLogo/>
+      <div className="navWrapper" ref={navRef}>
+        <div className="navBg" ref={bgRef}></div>
+        <a href="#beranda" className="navItem">Beranda</a>
+        <a href="#product" className="navItem">Product</a>
+        <a href="#about" className="navItem">About</a>
+      </div>
+      <div className="navContact">
+        <a href="" className="navItem">Hubungi Kami</a>
+        <p>{tes}</p>
+      </div>
+    </nav>
+  );
+}
