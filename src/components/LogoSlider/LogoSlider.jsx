5b751e006d42e46a501d3a566eb7f393f0bf8811
Adistian Herlambang <adistian59@gmail.com>
2026-02-16 21:33:57 +0700

    banyak

---
+++ src/components/LogoSlider/LogoSlider.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react";
+
+import "./LogoSlider.css";
+import { Korudo, Campina, Aice, Gracia } from "../ProductLogo";
+
+export default function LogoSlider() {
+  const trackRef = useRef(null);
+
+  useEffect(() => {
+    const track = trackRef.current;
+
+    // clone semua child sekali
+    const children = [...track.children];
+    children.forEach(child => {
+      track.appendChild(child.cloneNode(true));
+    });
+
+  }, []);
+
+  return (
+    <div className="marquee">
+      <div className="marqueeTrack" ref={trackRef}>
+        <Korudo />
+        <Campina />
+        <Aice />
+        <Gracia />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ProductLogo.jsx
@@ -0,0 +1,25 @@
+function Korudo() {
+  return (
+    <img src="/Logo/Korudo.svg" alt="Korudo Logo"/>
+  )
+}
+
+function Campina() {
+  return(
+    <img src="/Logo/Campina.svg" alt="Campina Logo"/>
+  )
+}
+
+function Aice() {
+  return(
+    <img src="/Logo/Aice.svg" alt="Aice Logo"/>
+  )
+}
+
+function Gracia() {
+  return(
+    <img src="/Logo/Gracia.svg" alt="Gracia Logo"/>
+  )
+}
+
+export { Korudo, Campina, Aice, Gracia }
