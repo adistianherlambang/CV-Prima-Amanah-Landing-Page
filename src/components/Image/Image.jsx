99039b4542e7d6eee778f3782d9c18aa6cb2643a
Adistian Herlambang <adistian59@gmail.com>
2026-03-03 23:50:44 +0700

    image responsive done

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,41 +1,16 @@
-import "./Image.css"
+import "./Image.css";
 
 export default function Image() {
-  return(
-    <div style={{
-      width: "100%",
-      display: "flex",
-      gap: "1rem"
-    }}>
-      <div style={{
-        width: "50%",
-        display: "flex",
-        gap: "1rem"
-      }}>
-        <img style={{
-          width: "100%",
-          borderRadius: "1rem"
-        }} src="/image/img1.webp" alt="photos" />
-        <img style={{
-          width: "100%",
-          borderRadius: "1rem"
-        }} src="/image/img2.webp" alt="photos" />
+  return (
+    <div className="imageContainer">
+      <div className="imageLeft">
+        <img id="image1" src="/image/img1.webp" alt="photos" />
+        <img id="image2" src="/image/img2.webp" alt="photos" />
       </div>
-      <div style={{
-        width: "50%",
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem"
-      }}>
-        <img style={{
-          width: "100%",
-          borderRadius: "1rem"
-        }} src="/image/img3.webp" alt="photos" />
-        <img style={{
-          width: "100%",
-          borderRadius: "1rem"
-        }} src="/image/img4.webp" alt="photos" />
+      <div className="imageRight">
+        <img id="image3" src="/image/img3.webp" alt="photos" />
+        <img id="image4" src="/image/img4.webp" alt="photos" />
       </div>
     </div>
-  )
+  );
 }
